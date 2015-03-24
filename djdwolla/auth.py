dabf08e61c3335591c50fa6375f2c0cb52f3cefe
Ben <benxv@yahoo.com>
2015-03-23 21:40:06 -0700

    Oauth working with constants, refactor rest of constants

--- djdwolla/auth.py
+++ djdwolla/auth.py
@@ -9,17 +9,12 @@ from dwolla import constants
 
 if settings.DWOLLA_SANDBOX:
     KEY = 'sandbox'
-    # admin_constants = constants
     DWOLLA_ADMIN_ACCOUNT = settings.DWOLLA_ACCOUNTS[KEY]
     DWOLLA_ADMIN_ID = settings.DWOLLA_ACCOUNTS['sandbox']['user_id']
-    DWOLLA_ADMIN_APP = None
 else:
     KEY = 'production'
-    # admin_constants = constants
     DWOLLA_ADMIN_ACCOUNT = settings.DWOLLA_ACCOUNTS['admin']
     DWOLLA_ADMIN_ID = DWOLLA_ADMIN_ACCOUNT['user_id']
-    # admin_constants.client_id = DWOLLA_ADMIN_ACCOUNT['key']
-    # admin_constants.client_secret = DWOLLA_ADMIN_ACCOUNT['secret']
 
 constants.client_id = settings.DWOLLA_ACCOUNTS[KEY]['key']
 constants.client_secret = settings.DWOLLA_ACCOUNTS[KEY]['secret']
@@ -27,10 +22,6 @@ constants.sandbox = settings.DWOLLA_SANDBOX
 
     
 DWOLLA_ACCOUNT = settings.DWOLLA_ACCOUNTS[KEY]
-# DWOLLA_APP = DwollaClientApp(DWOLLA_ACCOUNT['key'], DWOLLA_ACCOUNT['secret'])
-# if DWOLLA_ADMIN_APP is None:
-#     DWOLLA_ADMIN_APP = DWOLLA_APP
-# DWOLLA_GATE = DwollaGateway(DWOLLA_ACCOUNT['key'], DWOLLA_ACCOUNT['secret'])
 
 
 def set_constants(admin=False):
@@ -38,13 +29,6 @@ def set_constants(admin=False):
         constants.client_id = settings.DWOLLA_ACCOUNTS[KEY]['key']
         constants.client_secret = settings.DWOLLA_ACCOUNTS[KEY]['secret']
     else:
-        constants.client_id = DWOLLA_ADMIN_ACCOUNT['key']
-        constants.client_secret = DWOLLA_ADMIN_ACCOUNT['secret']
-    return constants
-
-
-def admin_constants(constants):
-    if KEY == 'production':
         constants.client_id = DWOLLA_ADMIN_ACCOUNT['key']
         constants.client_secret = DWOLLA_ADMIN_ACCOUNT['secret']
     return constants
